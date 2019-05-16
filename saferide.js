1b53884dd5049bd75ce3d4b47853143a978a73ae
juliandailey <50114021+juliandailey@users.noreply.github.com>
2019-05-15 19:19:22 -0500

    Add files via upload

---
+++ saferide.js
@@ -0,0 +1,72 @@
+
+let displayu = true;
+  let displayl = true;
+  let displaysr = true;
+  let displaycta = true;
+  
+/*
+function setUp(){
+    let visitedu = false;
+    let visitedl = false;
+    let visitedsr = false;
+    let visitedcta = false;
+    for (let i = 1; i < 5; i++){
+      if (displayu && !visitedu){
+        document.getElementById("option"+i).innerHTML = <p>Uber</p><img class="logo" src="images/uber logo.png">;
+        visitedu = true;
+      }
+      else if (displayl && !visitedl){
+        document.getElementById("option"+i).innerHTML = <p>Lyft</p><img class="logo" src="images/lyft logo.png">;
+        visitedl = true;
+      }
+      else if (displaysr && !visitedsr){
+        document.getElementById("option"+i).innerHTML = <p>SafeRide</p><img class="logo" src="images/saferide logo.jpg">;
+        visitedsr = true;
+      }
+      else if (displaycta && !visitedcta){
+        document.getElementById("option"+i).innerHTML = <p>CTA</p><img class="logo" src="images/cta logo.png">;
+        visitedcta = true;
+      } 
+  }
+}
+
+  function updateTime(){
+    let theTime = new Date();
+    let currTime = theTime.getHours() + ":" + theTime.getMinutes();
+    document.getElementById("time").innerHTML = currTime;
+  }
+
+  function checku(){
+    if (displayu)
+      displayu = false;
+    else
+      displayu = true;
+  }
+
+  function checkl(){
+    if (displayl)
+      displayl = false;
+    else
+      displayl = true;
+  }
+
+  function checksr(){
+    if (displaysr)
+      displaysr = false;
+    else
+      displaysr = true;
+  }
+
+  function checkcta(){
+    if (displaycta)
+      displaycta = false;
+    else
+      displaycta = true;
+  }
+
+function goButton(e){
+          let whereFrom = document.getElementById("from").value;
+          let whereTo = document.getElementById("to").value;
+          //plug whereFrom and whereTo into the google map widget
+}
+*/
